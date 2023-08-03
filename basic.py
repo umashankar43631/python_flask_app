ee4bfb697ad3d243112a160a85b541521974971e
Mulakalapalli Shankar <55424497+umashankar43631@users.noreply.github.com>
2023-08-03 11:13:26 +0530

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/<string:name>", methods=['GET'])
 def hello(name):
-    return f"<H1>Welcome to this website {name}</h1>"
+    return f"<H1> My name is {name}</h1>"
 
 if __name__=="__main__":
     app.run(port=5000,debug=True)
