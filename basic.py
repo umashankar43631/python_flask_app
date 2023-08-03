dd7c6901f4b9f21981c806b0e50ba7d26309ce25
shankar <shankar.mulakalapalli@gmail.com>
2023-08-03 11:02:51 +0530

    Commiting the basic flask file to git

---
+++ basic.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/<string:name>", methods=['GET'])
+def hello(name):
+    return f"<H1>Welcome to this website {name}</h1>"
+
+if __name__=="__main__":
+    app.run(port=8000,debug=True)
+
